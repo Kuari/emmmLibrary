3159d5ac2e94fa1633cd29cacda556998934f738
kuari <kuari@justmylife.cc>
2019-11-23 14:47:22 +0800

    first commit

---
+++ vue/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="app">
+
+        <el-container>
+            <el-header>
+                <themenu></themenu>
+            </el-header>
+            <el-main>
+                <router-view transition="fade"></router-view>
+            </el-main>
+        </el-container>
+
+    </div>
+</template>
+
+<script>
+import Menu from './components/Menu.vue'
+
+export default {
+    name: 'app',
+    components: {
+        'themenu': Menu
+    },
+    methods: {
+    }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-bottom: 60px;
+}
+.fade-enter-active, .fade-leave-active {
+    transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+    opacity: 0;
+}
+</style>
