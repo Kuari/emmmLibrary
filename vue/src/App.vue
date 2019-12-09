590a570cbbc5c28a3178415093184f2d0ddbe471
kuari <kuari@justmylife.cc>
2019-12-09 13:26:22 +0800

    更新vue，集成了eslint prettier iconfont normalize

---
+++ vue/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <el-container>
+      <el-header>
+        <hello />
+      </el-header>
+      <el-main>
+        <router-view transition="fade"></router-view>
+      </el-main>
+    </el-container>
+  </div>
+</template>
+
+<script>
+import hello from "./components/HelloWorld";
+
+export default {
+  name: "app",
+  components: {
+    hello
+  },
+  methods: {}
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-bottom: 60px;
+}
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+</style>
